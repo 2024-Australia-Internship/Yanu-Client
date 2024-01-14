2ea152583804b39f078a561fc696d5fd35efb0b4
seohyunCho <shanny06@naver.com>
2024-01-14 19:57:26 +0900

    ✨로그인 구현

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 function checkInfo(){
     alert('정보 확인');
+
+    let idValue = document.getElementsByClassName('id-input')[0].value;
+    let pwValue = document.getElementsByClassName('pw-input')[0].value;
+
+    const req = {
+        user_email: idValue,
+        user_pw: pwValue
+    }
+    axios.post(`${BASE_URL}/users/login`, req)
+    .then(response => {
+        console.log(response)
+    })
+    .catch(error => {
+        console.error('There has been a problem with your axios request:', error);
+    });
 }
