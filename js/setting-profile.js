b8f0acbf662e1348cce4ce7c4096262dcd7123d2
seohyunCho <shanny06@naver.com>
2024-01-16 13:06:44 +0900

    ✨프로필 이미지 보내기 구현

--- js/setting-profile.js
+++ js/setting-profile.js
@@ -8,6 +8,7 @@ fileUpload.addEventListener('change', getImageFiles);
 
 function getImageFiles(e){
     const file = e.currentTarget.files[0];
+    console.log(file);
 
     if (!file.type.match("image/.*")) {
         alert('이미지 파일만 업로드가 가능합니다.');
@@ -20,4 +21,36 @@ function getImageFiles(e){
 
     img.src = URL.createObjectURL(file); 
     parent.replaceChildren(img);
+}
+
+function settingProfile(){
+    const formData = new FormData();
+
+    let file = document.getElementsByClassName('upload-file')[0].files[0];
+    console.log(file);
+
+    formData.append('profile_image', file);
+
+    const config = {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+    };
+
+    let nickname = document.getElementsByClassName('nickname-input')[0].value;
+    let introduction = document.getElementsByClassName('introduction-input')[0].value;
+
+    if(nickname == '') return alert('input your nickname');
+    if(introduction == '') return alert('input your introduction');
+
+    axios.post(`${BASE_URL}/users/profile`, formData, config)
+    .then(response => {
+        console.log(response);
+        alert('이미지 업로드 성공');
+    })
+    .catch(error => {
+        console.error('There has been a problem with your axios request:', error);
+        alert('이미지 업로드 실패');
+    });
+
 }
