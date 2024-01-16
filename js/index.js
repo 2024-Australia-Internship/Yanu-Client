eeb83d463214ff7d7eca9d140ada9ec0f0e0352e
seohyunCho <shanny06@naver.com>
2024-01-16 11:47:24 +0900

    ✨로그인 페이지 비밀번호 보기 구현

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
+function showMyPw(){
+    let myPw = document.getElementsByClassName('my-pw-input')[0];
+    let toggleBtn = document.getElementsByClassName('eye-div')[0];
+
+    if(myPw.type === "password"){
+        myPw.type = "text";
+        toggleBtn.innerHTML = `<iconify-icon icon="ph:eye-closed-light" class="my-pw-icon pw-icon" onclick="showMyPw()"></iconify-icon>`
+    }else{
+        myPw.type = "password";
+        toggleBtn.innerHTML = `<img src="/images/eye-open.svg" class="my-pw-icon pw-icon" onclick="showMyPw()">`;
+    }
+}
+
 function checkInfo(){
     alert('정보 확인');
 
     let idValue = document.getElementsByClassName('id-input')[0].value;
     let pwValue = document.getElementsByClassName('pw-input')[0].value;
+
+    console.log(pwValue);
 
     const req = {
         user_email: idValue,
@@ -17,5 +32,6 @@ function checkInfo(){
     .catch(error => {
         console.error('There has been a problem with your axios request:', error);
         alert('로그인 실패');
+        if(error.response.status) alert('비밀번호가 일치하지 않음');
     });
 }
