cb78fc4a3a576f46c2bc357e60fe69c9b4d9a266
seohyunCho <shanny06@naver.com>
2024-01-17 21:47:01 +0900

    💄favorites 화면 구현

--- js/favorites-page.js
+++ js/favorites-page.js
@@ -34,9 +34,125 @@ for(let i = 0; i<20; i++){
     productDiv.appendChild(product);
 }
 
+let favoritesProductsDiv = document.getElementsByClassName('favorites-products-div')[0];
+
+for(let i = 0; i<20; i++){
+    let product = document.createElement('div');
+    product.className = 'favorites-product';
+
+    let productDetailDiv = document.createElement('div');
+    productDetailDiv.className = 'favorites-product-detail-div';
+
+    let productName = document.createElement('div');
+    productName.className = 'favorites-product-name';
+    productName.innerText = "Fresh carrots!"
+
+    let productFarmName = document.createElement('div');
+    productFarmName.className = 'favorites-product-farm-name';
+    productFarmName.innerText = "Annie's Farm";
+
+    let productDetail = document.createElement('div');
+    productDetail.className = "favorites-product-detail";
+
+    let productPriceDiv = document.createElement('div');
+    productPriceDiv.className = "favorites-product-price-div";
+
+    let productPrice = document.createElement('div');
+    productPrice.className = "favorites-product-price";
+    productPrice.innerText = '$ 19';
+
+    let productUnit = document.createElement('div');
+    productUnit.className = "favorites-product-unit";
+    productUnit.innerText = ' / kg';
+
+    productPriceDiv.appendChild(productPrice);
+    productPriceDiv.appendChild(productUnit);
+
+    productDetail.appendChild(productPriceDiv);
+    productDetail.innerHTML += '<iconify-icon icon="ph:heart-fill" class="favorites-heart-btn"></iconify-icon>';
+
+    productDetailDiv.appendChild(productName);
+    productDetailDiv.appendChild(productFarmName);
+    productDetailDiv.appendChild(productDetail);
+
+    product.innerHTML = '<img src="/images/product-img.png" class="favorites-product-img">';
+    product.appendChild(productDetailDiv);
+
+    favoritesProductsDiv.appendChild(product);
+}
+
+let favoritesFarmsDiv = document.getElementsByClassName('favorites-farms-div')[0];
+
+for(let i = 0; i<20; i++){
+
+    let favoritesFarm = document.createElement('div');
+    favoritesFarm.classList.add('farm');
+    favoritesFarm.classList.add('favorites-farm');
+
+    let farmDetailDiv = document.createElement('div');
+    farmDetailDiv.className = "farm-detail-div";
+
+    let farmName = document.createElement('div');
+    farmName.className = 'farm-username';
+    farmName.innerText = "Annie's Farm";
+
+    let farmUsername = document.createElement('div');
+    farmUsername.className = 'farm-username';
+    farmUsername.innerText = "Annie Johnson";
+
+    let farmReviewDiv = document.createElement('div');
+    farmReviewDiv.className = 'farm-review-div';
+
+    let farmReviewDetail = document.createElement('div');
+    farmReviewDetail.className = 'farm-review-detail';
+
+    let farmProducts = document.createElement('div');
+    farmProducts.className = 'farm-products';
+    farmProducts.innerText = '120 products'
+
+    let farmReviews = document.createElement('div');
+    farmReviews.className = 'farm-reviews';
+    farmReviews.innerText = '23 review'
+
+    let farmReviewHearts = document.createElement('div');
+    farmReviewHearts.className = 'farm-review-hearts';
+
+    let farmReviewHeartCnt = document.createElement('div');
+    farmReviewHeartCnt.className = 'farm-review-heart-cnt';
+    farmReviewHeartCnt.innerText = '1,232'
+
+    farmReviewHearts.innerHTML = `<iconify-icon icon="ph:heart-fill" class="farm-review-heart-icon"></iconify-icon>`;
+    farmReviewHearts.appendChild(farmReviewHeartCnt);
+
+    farmReviewDetail.appendChild(farmProducts);
+    farmReviewDetail.appendChild(farmReviews);
+    farmReviewDetail.appendChild(farmReviewHearts);
+
+    farmReviewDiv.appendChild(farmReviewDetail);
+    farmReviewDiv.innerHTML += `<iconify-icon icon="ph:heart-fill" class="farm-heart-icon"></iconify-icon>`;
+
+    farmDetailDiv.appendChild(farmName);
+    farmDetailDiv.appendChild(farmUsername);
+    farmDetailDiv.appendChild(farmReviewDiv);
+
+    favoritesFarm.innerHTML = `<img src="/images/product-img.png" class="farm-img">`
+    favoritesFarm.appendChild(farmDetailDiv);
+
+    favoritesFarmsDiv.appendChild(favoritesFarm);
+}
+
 let heartBtns = [...document.getElementsByClassName('heart-btn')];
 heartBtns.forEach((e) => {
     e.onclick = (e) => heartProduct(e);
+});
+let favoritesHeartBtns = [...document.getElementsByClassName('favorites-heart-btn')];
+favoritesHeartBtns.forEach((e) => {
+    e.onclick = (e) => FavoriteHeartProduct(e);
+});
+
+let farmHeartBtns = [...document.getElementsByClassName('farm-heart-icon')];
+farmHeartBtns.forEach((e) => {
+    e.onclick = (e) => FavoriteHeartProduct(e);
 });
 
 function heartProduct(e){
@@ -47,5 +163,40 @@ function heartProduct(e){
     }else{
         e.target.classList.add('choose-heart-btn');
         e.target.icon = 'ph:heart-fill';
+    }
+}
+
+function FavoriteHeartProduct(e){
+    console.log(e.target);
+    if(e.target.classList.contains('choose-heart-btn')){
+        e.target.classList.remove('choose-heart-btn');
+        e.target.icon = 'ph:heart-fill';
+    }else{
+        e.target.classList.add('choose-heart-btn');
+        e.target.icon = 'ph:heart';
+    }
+}
+
+function chooseFavoritesCategory(category){
+    let products = document.getElementsByClassName('favorites-products-div')[0];
+    let farms = document.getElementsByClassName('favorites-farms-div')[0];
+
+    let productsBtn = document.getElementsByClassName('products-btn')[0];
+    let farmsBtn = document.getElementsByClassName('farms-btn')[0];
+    
+    console.log(productsBtn);
+    console.log(farmsBtn);
+    if(category){ //products
+        products.style.display = 'grid';
+        farms.style.display = 'none';
+
+        productsBtn.classList.add('choose-category-btn')
+        farmsBtn.classList.remove('choose-category-btn')
+    }else{ //farms
+        farms.style.display = 'grid';
+        products.style.display = 'none';
+
+        farmsBtn.classList.add('choose-category-btn')
+        productsBtn.classList.remove('choose-category-btn')
     }
 }
