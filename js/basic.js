df27fd8a82bddf983f1c74c3d02cd23964414af4
seohyunCho <shanny06@naver.com>
2024-01-23 22:54:11 +0900

    🎨farvorites 토글 버튼 js 파일 이동

--- js/basic.js
+++ js/basic.js
@@ -3,3 +3,14 @@ function getCookie(key){
     key = new RegExp(key + '=([^;]*)'); 
     return key.test(document.cookie) ? unescape(RegExp.$1) : '';
 }
+
+
+function heartToggle(e){
+    if(e.target.classList.contains('choose-heart-btn')){
+        e.target.classList.remove('choose-heart-btn');
+        e.target.icon = 'ph:heart';
+    }else{
+        e.target.classList.add('choose-heart-btn');
+        e.target.icon = 'ph:heart-fill';
+    }
+}
