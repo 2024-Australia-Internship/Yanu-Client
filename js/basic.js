74cc9435cd481216fc7d5370cf5ced5589b741f7
seohyunCho <shanny06@naver.com>
2024-01-24 20:51:43 +0900

    🎨heartToggle js 파일 이동

--- js/basic.js
+++ js/basic.js
@@ -6,11 +6,36 @@ function getCookie(key){
 
 
 function heartToggle(e){
+    console.log(e.target.parentNode.parentNode.parentNode.id);
+    let product_code = e.target.parentNode.parentNode.parentNode.id;
+    console.log(e.target.parentNode.parentNode.parentNode.classList[1]);
+    let user_code = e.target.parentNode.parentNode.parentNode.classList[1];
+    console.log(!e.target.classList.contains('product-btn'));
+    let product_category = !e.target.classList.contains('product-btn');
+
     if(e.target.classList.contains('choose-heart-btn')){
-        e.target.classList.remove('choose-heart-btn');
-        e.target.icon = 'ph:heart';
+        axios.delete(`${BASE_URL}/hearts/${user_code}/${product_code}`)
+        .then(response => {
+            console.log(response);
+            e.target.classList.remove('choose-heart-btn');
+            e.target.icon = 'ph:heart';
+        })
+        .catch(error => {
+            console.error('There has been a problem with your axios request:', error);
+        });
     }else{
-        e.target.classList.add('choose-heart-btn');
-        e.target.icon = 'ph:heart-fill';
+        const req = {
+            user_code: user_code,
+            product_category: product_category
+        }
+        axios.post(`${BASE_URL}/hearts/${product_code}`, req)
+        .then(response => {
+            console.log(response);
+            e.target.classList.add('choose-heart-btn');
+            e.target.icon = 'ph:heart-fill';
+        })
+        .catch(error => {
+            console.error('There has been a problem with your axios request:', error);
+        });
     }
 }
