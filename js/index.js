8b3592dbdedebbeb0f2b0ed73dea419617bef7dc
seohyunCho <shanny06@naver.com>
2024-04-21 03:18:31 +0900

    ✨users API 연결

--- js/index.js
+++ js/index.js
@@ -15,36 +15,17 @@ function checkInfo(){
 
     let idValue = document.getElementsByClassName('id-input')[0].value;
     let pwValue = document.getElementsByClassName('my-pw-input')[0].value;
-
-    console.log(pwValue);
-
     const req = {
-        user_email: idValue,
-        user_pw: pwValue
+        email: idValue,
+        password: pwValue
     }
     
     axios.post(`${BASE_URL}/users/login`, req)
-    .then(response => {
-        console.log(response);
-        
-        alert('로그인 성공');
-        saveUserInfo(response.data.result);
+    .then(() => {
+        window.location.href = '/html/main-page.html'
     })
     .catch(error => {
         console.error('There has been a problem with your axios request:', error);
-        alert('로그인 실패');
-        if(error.response.status == 404) alert('비밀번호가 일치하지 않음');
+        alert(error.response.data.message);
     });
-}
-
-function saveUserInfo(result){
-    let user_code = result.user_code;
-
-    let expireDate = new Date();
-    expireDate.setMonth(expireDate.getMonth() + 1);
-    document.cookie = `user_code=${user_code}; path=/; expires=` + expireDate.toGMTString();
-    
-    console.log(document.cookie);
-
-    window.location.href = `/html/main-page.html`;
 }
