c1764c98ce3ca096dc404ecdcfcbe7a8e144e67d
seohyunCho <shanny06@naver.com>
2024-04-29 01:34:54 +0900

    🔇콘솔로그 삭제

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,5 @@
 let choosePage = document.getElementById('choose-page');
 let choosePageName = choosePage.firstElementChild.classList[1];
-console.log(choosePage.firstElementChild.innerHTML);
 choosePage.firstElementChild.innerHTML = `<img src="/images/${choosePageName}-hover.svg">`;
 
 function isFarmer(){
@@ -8,7 +7,6 @@ function isFarmer(){
 
     axios.get(`${BASE_URL}/users/${user_code}`)
     .then(response => {
-        console.log(response);
         if(response.data.userAllInfo[0].is_farmer){
             window.location.href = '/html/my-page-farmer.html'
         }else{
