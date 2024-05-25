b60748e39200e61c0e0483a9db92af1460f9a5c9
seohyunCho <shanny06@naver.com>
2024-05-25 21:05:05 +0900

    Fix: 오타 수정

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,12 @@
 let choosePage = document.getElementById('choose-page');
 let choosePageName = choosePage.firstElementChild.classList[1];
 choosePage.firstElementChild.innerHTML = `<img src="/images/${choosePageName}-hover.svg">`;
+
 function isFarmer(){
     let isFarmer = JSON.parse(getCookie('userdata')).is_farmer
     if(isFarmer){
         window.location.href = '/html/my-page-farmer.html'
     }else{
-        window.location.href = '/html/my-page-farmer.html';
+        window.location.href = '/html/my-page-admin.html';
     }
 }
