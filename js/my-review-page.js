cd5fbc65bcd66778d476547e9cef046d3af3753e
seohyunCho <shanny06@naver.com>
2024-06-03 22:58:16 +0900

    Feat: api 연결을 위해 js로 퍼블리싱

--- js/my-review-page.js
+++ js/my-review-page.js
@@ -17,6 +17,137 @@ document.addEventListener('click', (e) => {
 
 });
 
+window.onload = () => {
+    getReview();
+}
+
+async function getReview() {
+    try{
+        // const response = await axios.get(`${BASE_URL}/reviews`, config);
+        // console.log(response);
+
+        const response = [
+            {
+                "userId": 2,
+                "productId": 8,
+                "starrating": 5,
+                "content": "좋은데... 뭔가 아쉬워요 씁하",
+                "createdAt": "2024-06-02"
+            },
+            {
+                "userId": 2,
+                "productId": 8,
+                "starrating": 1,
+                "content": "좋은데... 뭔가 아쉬워요 씁하",
+                "createdAt": "2024-06-02"
+            },
+            {
+                "userId": 2,
+                "productId": 8,
+                "starrating": 2,
+                "content": "좋은데... 뭔가 아쉬워요 씁하",
+                "createdAt": "2024-06-03"
+            },
+            {
+                "userId": 2,
+                "productId": 8,
+                "starrating": 3,
+                "content": "좋은데... 뭔가 아쉬워요 씁하",
+                "createdAt": "2024-06-03"
+            }
+        ]
+
+        let reviewHistory = {};
+        response.forEach(history => {
+            if(reviewHistory[history.createdAt]) {
+                reviewHistory[history.createdAt].push(history);
+            }else{
+                reviewHistory[history.createdAt] = [history];
+            }
+        })
+
+        showReviews(reviewHistory);
+    }catch(error){
+        console.error(error);
+    }
+}
+
+function showReviews(reviewHistory) {
+    const keys = Object.keys(reviewHistory);
+    const myReviewHistory = document.getElementsByClassName('my-review-list')[0];
+
+    keys.forEach(key => {
+        let value = reviewHistory[key];
+
+        let currentDateReview = document.createElement('div');
+        currentDateReview.className = 'current-date-review';
+
+        let reviewTitle = document.createElement('div');
+        reviewTitle.className = 'review-title';
+        reviewTitle.innerText = key.replaceAll('-', '.');
+
+        let currentDateReviewList = document.createElement('div');
+        currentDateReviewList.className = 'current-date-review-list';
+
+        currentDateReview.appendChild(reviewTitle);
+        currentDateReview.appendChild(currentDateReviewList);
+
+        myReviewHistory.appendChild(currentDateReview)
+
+        value.forEach(product => {
+            let reviewCard = document.createElement('div');
+            reviewCard.className = 'review-card';
+
+            let reviewStarEditBox = document.createElement('div');
+            reviewStarEditBox.className = 'review-star-edit-box';
+
+            let starRating = document.createElement('div');
+            starRating.className = 'star-rating';
+            starRating.innerHTML = `${'<iconify-icon icon="ph:star-fill" class="star"></iconify-icon>'.repeat(product.starrating)}`
+        
+            let editBtn = document.createElement('iconify-icon');
+            editBtn.className = 'edit-btn'
+            editBtn.icon = 'quill:meatballs-h'
+
+            reviewStarEditBox.appendChild(starRating)
+            reviewStarEditBox.appendChild(editBtn)
+
+            let productName = document.createElement('div');
+            productName.className = 'product-name';
+
+            let div = document.createElement('div');
+            div.innerText = '[Annie] SWEET SWEET strawberries 🍓 sdhfk sdfhjshfhdf hsdfkhsjkdfhshdf sdhfkjh'
+
+            let productBtn = document.createElement('iconify-icon');
+            productBtn.className = 'product-btn';
+            productBtn.icon = 'iconamoon:arrow-up-2-thin';
+
+            productName.appendChild(div)
+            productName.appendChild(productBtn)
+
+            let myReview = document.createElement('div');
+            myReview.className = 'my-review';
+
+            let reviewImg = document.createElement('img');
+            reviewImg.className = 'review-img';
+            reviewImg.src = '/images/product-img.png'
+
+            let review = document.createElement('div');
+            review.className ='review'
+            review.innerText = 'These strawberries are really GOOOOOOOD!! I have two sons and my sons love these strawberries...'
+        
+            myReview.appendChild(reviewImg)
+            myReview.appendChild(review)
+
+            reviewCard.appendChild(reviewStarEditBox)
+            reviewCard.appendChild(productName)
+            reviewCard.appendChild(myReview)
+
+            currentDateReviewList.appendChild(reviewCard)
+        })
+    })
+}
+
 function showProduct(e, i){
     window.location.href = '../html/product-page.html';
 }
