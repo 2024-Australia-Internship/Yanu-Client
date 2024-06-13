c5915c4cfc716d3a5bc64fa9b9f0d5ebe3c866a9
seohyunCho <shanny06@naver.com>
2024-06-13 22:10:07 +0900

    Feat: 리뷰 수정하기 request 생성

--- js/edit-my-review.js
+++ js/edit-my-review.js
@@ -1,5 +1,7 @@
 const urlParams = new URL(location.href).searchParams;
 const product_id = urlParams.get('product_id');
+const review_id = urlParams.get('review_id');
+let recentImages = [];
 
 const starArr = [...document.getElementsByClassName('star')];
 
@@ -7,8 +9,71 @@ window.onload = () => {
     starArr.forEach((value, index) => {
         value.addEventListener('click', () => starRating(index));
     });
+    getMyReview()
 }
 
+async function getMyReview(){
+    try{
+        const response = await axios.get(`${BASE_URL}/reviews/8`, config);
+        
+        console.log()
+        editInfo(response.data)
+    }catch(err){
+        console.error(err);
+    }
+}
+
+function setReview(){
+    try{
+        let starrating = document.getElementsByClassName('fill-star').length;
+        let content = document.getElementsByClassName('evaluation-box')[0].value;
+        let images = [...document.getElementsByClassName('image')];
+        let files = document.getElementsByClassName('upload-file')[0].files;
+        let removeImage = [...recentImages];
+
+        if(starrating === 0) {
+            return alert('Please enter a rating')
+        }
+    
+        if(content === ''){
+            return alert('Please enter a content')
+        }
+
+        let formData = new FormData();
+
+        // 지운 사진 조회
+        images.forEach(image => {
+            if(removeImage.includes(image)){
+                removeImage.splice(removeImage.indexOf(image), 1);
+            } 
+        })
+        formData.append('removeImage', removeImage.join(', '))
+
+        // 새로 업로드한 파일 추가
+        for(let file of files){
+            formData.append('image', file);
+        }
+
+        formData.append('starrating', starrating)
+        formData.append('content', content)
+    }catch(err){
+        console.error(err);
+    }
+}
+
+function editInfo(review){
+    console.log(review);
+    let descriptionBox = document.getElementsByClassName('evaluation-box')[0]
+    descriptionBox.value = review.content;
+
+    starArr[review.starrating - 1].click();
+
+    let images = review.images.map(img => `${IMAGE_URL}${img}`);
+    recentImages = [...images];
+    images.forEach(image => {
+        createElement(image);
+    })
+}
 
 function starRating(index){
     for(let star of starArr){
@@ -31,20 +96,20 @@ function getImageFiles(e) {
     let images = [...document.getElementsByClassName('upload-img-li')].length;
     const files = e.currentTarget.files;
   
-    if ([...files].length >= 6 && [...files].length + images >= 6) {
-      alert('이미지는 최대 5개까지 업로드가 가능합니다.');
+    if ([...files].length >= 6 || [...files].length + images >= 6) {
+      alert('Up to 5 images can be uploaded.');
       return;
     }
 
     [...files].forEach(file => {
         if (!file.type.match("image/.*")) {
-          alert('이미지 파일만 업로드가 가능합니다.');
+          alert('Only image files can be uploaded.');
           return;
         }
         if ([...files].length < 6 && [...files].length + images < 6) {
             const reader = new FileReader();
             reader.onload = (e) => {
-              createElement(e, file);
+              createElement(e.target.result);
             };
             reader.readAsDataURL(file);
         }
@@ -52,14 +117,19 @@ function getImageFiles(e) {
 
 }
 
-function createElement(e, file){
+function createElement(image){
     let uploadImgDiv = document.getElementsByClassName('upload-img-div')[0];
     
     let uploadImgLi = document.createElement('div');
     uploadImgLi.classList.add('upload-img');
     uploadImgLi.classList.add('upload-img-li');
     uploadImgLi.innerHTML += `<iconify-icon icon="mynaui:x" class="delete-img" onclick="deleteImg(this)"></iconify-icon>`
-    uploadImgLi.style.backgroundImage = `url(${e.target.result})`
+    
+    let newimage = document.createElement('img');
+    newimage.className = 'image';
+    newimage.src = image;
+
+    uploadImgLi.appendChild(newimage);
 
     uploadImgDiv.appendChild(uploadImgLi);
     let images = [...document.getElementsByClassName('upload-img-li')].length;
