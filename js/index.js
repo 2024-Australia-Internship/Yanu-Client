bb91c17bf7b77bb4e1be7d6058b6f75cdb460840
seohyunCho <shanny06@naver.com>
2024-06-15 01:00:33 +0900

    Fix: 오타 수정

--- js/index.js
+++ js/index.js
@@ -40,13 +40,13 @@ async function checkInfo(){
 
         // 유저 정보 저장
         const getInfo = await axios.get(`${BASE_URL}/users`, config)
-        const { id, is_farmer, nickname, profile_image } = getInfo.data;
+        const { id, is_farmer, nickname, profile } = getInfo.data;
 
         const data = {
             id: id,
             is_farmer: is_farmer,
             nickname: nickname,
-            profile_image: profile_image,
+            profile_image: profile,
         }
 
         const jsonData = JSON.stringify(data);
